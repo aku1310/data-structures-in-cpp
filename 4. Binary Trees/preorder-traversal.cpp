2b0fc7af70996af5fe20f263990d294c5041f0b5
Akanksha Kushwaha <cmakanksha@gmail.com>
2023-06-13 14:31:41 +0530

    add: add tree traversal programs
    Signed-off-by: Akanksha Kushwaha <cmakanksha@gmail.com>

---
+++ 4. Binary Trees/preorder-traversal.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<int> preorderTraversal(TreeNode *root)
+{
+    vector<int> preorder;
+    stack<TreeNode *> st;
+
+    if (root == NULL)
+        return preorder;
+
+    st.push(root);
+
+    while (!st.empty())
+    {
+        TreeNode *top = st.top();
+        st.pop();
+        preorder.push_back(top->val);
+
+        if (top->right != NULL)
+            st.push(top->right);
+
+        if (top->left != NULL)
+            st.push(top->left);
+    }
+    return preorder;
+}
