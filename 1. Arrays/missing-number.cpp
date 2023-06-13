c204177767832d7afdec5070453101032fd44b28
Akanksha Kushwaha <cmakanksha@gmail.com>
2023-06-13 19:11:59 +0530

    update: add array program
    Signed-off-by: Akanksha Kushwaha <cmakanksha@gmail.com>

---
+++ 1. Arrays/missing-number.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingNumber(vector<int> &nums)
+{
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < nums.size(); ++i)
+        if (nums[i] != i)
+            return i;
+
+    return nums.size();
+}
