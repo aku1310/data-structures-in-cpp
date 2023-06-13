f3acf2411a7b3f1aeffedec40986d347f0b2a79c
Akanksha Kushwaha <cmakanksha@gmail.com>
2023-06-13 19:11:44 +0530

    update: add another approach
    Signed-off-by: Akanksha Kushwaha <cmakanksha@gmail.com>

--- 1. Arrays/two-sum.cpp
+++ 1. Arrays/two-sum.cpp
@@ -1,13 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
-    for(int i = 0; i < nums.size() - 1; ++i){
-        for(int j = i + 1; j < nums.size(); ++j){
-            if(nums[i] + nums[j] == target)
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    for (int i = 0; i < nums.size() - 1; ++i)
+    {
+        for (int j = i + 1; j < nums.size(); ++j)
+        {
+            if (nums[i] + nums[j] == target)
                 return {i, j};
         }
     }
     
+    // OR
+
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (mp.find(target - nums[i]) == mp.end())
+            mp[nums[i]] = i;
+        else
+            return {mp[target - nums[i]], i};
+    }
     return {};
 }
